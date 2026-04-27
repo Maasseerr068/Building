4bb1fed5e6c49fabc42eece7031807fa9b70f809
Maasseerr068 <neldiab@gmail.com>
2026-04-27 21:40:06 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,7 +5,6 @@
 
 const SUPABASE_URL = 'https://wiekrapbativgdsjtfua.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_fEZyPsZV-0SCisSOQZjVWg_NijWBOJF';
-
 // الأكواد السرية للدخول (غيّرها بما تشاء)
 const ADMIN_PIN = '1234';
 const TREASURER_PIN = '5678';
